5d7fa74071bd4718b678c9210386a03f65489d25
CipherSamurai <63560659+CipherSamurai@users.noreply.github.com>
2020-05-13 05:21:55 +0000

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,22 @@
+/*
+ * helloWorld.c
+ * 
+ * Copyright 2020
+ * @author <!pher $@mur@!
+ * @date 5/13/2020
+ * 
+ * Displays hello world on console
+ * 
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello, World!");
+	return 0;
+}
+
